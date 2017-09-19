5abb15152100562f512dc8936a2e66282cf9a9e5
David Johnson <djohnson.m@gmail.com>
2017-09-19 19:22:58 +0000

    Initial commit of jsvalToValue fixes, QuickCheck tests added

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,15 +1,35 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NamedFieldPuns #-}
-
-
 module Main where
 
 import Test.Hspec (it, hspec, describe, shouldSatisfy, shouldBe, Spec)
 import Test.Hspec.Core.Runner (hspecResult, Summary(..))
+import Test.QuickCheck
 import Data.Aeson
+import GHCJS.Marshal
+import Control.Monad
+import Test.QuickCheck.Instances
 
 import Miso
+import Miso.FFI
+
+instance Arbitrary Value where
+  arbitrary = sized sizedArbitraryValue
+
+sizedArbitraryValue :: Int -> Gen Value
+sizedArbitraryValue n
+  | n <= 0 = oneof [pure Null, bool, number, string]
+  | otherwise = resize n' $ oneof [pure Null, bool, number, string, array, object']
+  where
+    n' = n `div` 2
+    bool = Bool <$> arbitrary
+    number = Number <$> arbitrary
+    string = String <$> arbitrary
+    array = Array <$> arbitrary
+    object' = Object <$> arbitrary
+
 
 main :: IO ()
 main = do
@@ -32,6 +52,10 @@ storageTests = describe "Storage tests" $ do
     setSessionStorage "foo" obj
     Right r <- getLocalStorage "foo"
     r `shouldBe` obj
+  it "Should round trip" $ do
+    property $ \x -> do
+      Just v <- jsvalToValue =<< toJSVal x
+      v `shouldBe` x
 
 phantomExit :: Int -> IO ()
 phantomExit x
